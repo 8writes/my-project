7a2a3c6379f1cec217e99141dd6565610637f812
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-05-14 22:06:27 +0100

    fix

--- src/views/contact/contact.jsx
+++ src/views/contact/contact.jsx
@@ -6,26 +6,34 @@ import PagesHeader from '@/components/pages-header'
 
 const Contact = () => {
   return (
-    <div className='projects-page  py-16 lg:pt-20'>
+    <div className='projects-page  lg:pt-20'>
       <PagesHeader title='Contact' />
-      <div className='font text-slate-700 pb-40 flex flex-wrap justify-center gap-20'>
-        <div>
+      <div className='font text-slate-700 pb-60 lg:pb-40 px-6 pt-20 flex flex-wrap justify-center gap-20'>
+        <div className='w-full lg:w-96'>
           {' '}
-                  <h1 className='text-2xl font-semibold'>Location</h1> <p>
-                      
-          </p>
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-xl font-semibold'>Location</h1>{' '}
+            <p className=' tracking-wide font-medium'>
+              <span className='font-semibold'> Head Office:</span> 132 Woji Road
+              GRA Phase II, Port Harcourt, Rivers State. <br />
+              <span className='font-semibold'>Branch:</span> Suite 109, 6 Sapele
+              Street, Off Ladoke Akinlola Boulevard, Garki 2, Abuja
+            </p>
+          </div>
         </div>
-        <div>
+        <div className='w-full lg:w-fit'>
           {' '}
-                  <h1 className='text-2xl font-semibold'>Call Center</h1> <p>
-                      
-          </p>
+          <div className='flex flex-col gap-4 justify-start'>
+            <h1 className='text-xl font-semibold'>Call Center</h1>{' '}
+            <p>+234 816 469 8085</p>
+          </div>
         </div>
-        <div>
+        <div className='w-full lg:w-fit'>
           {' '}
-                  <h1 className='text-2xl font-semibold'>Email Us</h1> <p>
-                      
-          </p>
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-xl font-semibold'>Email Us</h1>{' '}
+            <p>onpointsynergyltd@gmail.com</p>
+          </div>
         </div>
       </div>
     </div>
