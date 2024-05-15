29d0f907d56282e16cd30ac664e8b500ef6fb8b5
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-05-15 06:49:50 +0100

    fix

--- src/views/contact/contact.jsx
+++ src/views/contact/contact.jsx
@@ -15,7 +15,13 @@ const Contact = () => {
       <div className='projects-page font text-slate-700 pb-60 lg:pb-40 px-6 pt-20 flex flex-wrap justify-center gap-20'>
         <div className='w-full lg:flex justify-center  gap-5 items-center'>
           {' '}
-          <Image src={location} alt='' width={100} height={30} />
+          <Image
+            src={location}
+            alt=''
+            width={100}
+            height={30}
+            className='py-5'
+          />
           <div className='flex flex-col gap-4'>
             <h1 className='text-2xl font-semibold'>Location</h1>{' '}
             <p className=' tracking-wide font-medium text-xl leading-7'>
@@ -26,17 +32,24 @@ const Contact = () => {
             </p>
           </div>
         </div>
-        <div className='w-full lg:w-fit lg:flex  gap-5 items-center'>
+        <div className='w-full lg:w-fit lg:flex gap-5 items-center'>
           {' '}
-          <Image src={support} alt='' width={100} height={50} />
+          <Image
+            src={support}
+            alt=''
+            width={100}
+            height={50}
+            className='py-5'
+          />
           <div className='flex flex-col gap-4 justify-start'>
             <h1 className='text-2xl font-semibold'>Call Center</h1>{' '}
             <p className='font-medium text-xl'>+234 816 469 8085</p>
+            <p className='font-medium text-xl'>+234 808 864 1689</p>
           </div>
         </div>
         <div className='w-full lg:w-fit lg:flex gap-5 items-center'>
           {' '}
-          <Image src={email} alt='' width={100} height={30} />
+          <Image src={email} alt='' className='py-5 w-20' />
           <div className='flex flex-col gap-4'>
             <h1 className='text-2xl font-semibold'>Email Us</h1>{' '}
             <p className='font-medium text-xl'>onpointsynergyltd@gmail.com</p>
