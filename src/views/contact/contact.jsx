0125e11b6c0bd1347c48dbcbf44c4bb97c0b86c1
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-05-15 06:45:10 +0100

    fix

--- src/views/contact/contact.jsx
+++ src/views/contact/contact.jsx
@@ -3,36 +3,43 @@
 'use client'
 
 import PagesHeader from '@/components/pages-header'
+import Image from 'next/image'
+import location from '../../../public/images/location.png'
+import support from '../../../public/images/support.png'
+import email from '../../../public/images/email.png'
 
 const Contact = () => {
   return (
-    <div className='projects-page pt-16 lg:pt-20'>
+    <div className='bg-white pt-16 lg:pt-20'>
       <PagesHeader title='Contact' />
-      <div className='font text-slate-700 pb-60 lg:pb-40 px-6 pt-20 flex flex-wrap justify-center gap-20'>
-        <div className='w-full lg:w-96'>
+      <div className='projects-page font text-slate-700 pb-60 lg:pb-40 px-6 pt-20 flex flex-wrap justify-center gap-20'>
+        <div className='w-full lg:flex justify-center  gap-5 items-center'>
           {' '}
+          <Image src={location} alt='' width={100} height={30} />
           <div className='flex flex-col gap-4'>
-            <h1 className='text-xl font-semibold'>Location</h1>{' '}
-            <p className=' tracking-wide font-medium'>
+            <h1 className='text-2xl font-semibold'>Location</h1>{' '}
+            <p className=' tracking-wide font-medium text-xl leading-7'>
               <span className='font-semibold'> Head Office:</span> 132 Woji Road
-              GRA Phase II, Port Harcourt, Rivers State. <br />
+              GRA Phase II, Port Harcourt, Rivers State. <br />{' '}
               <span className='font-semibold'>Branch:</span> Suite 109, 6 Sapele
               Street, Off Ladoke Akinlola Boulevard, Garki 2, Abuja
             </p>
           </div>
         </div>
-        <div className='w-full lg:w-fit'>
+        <div className='w-full lg:w-fit lg:flex  gap-5 items-center'>
           {' '}
+          <Image src={support} alt='' width={100} height={50} />
           <div className='flex flex-col gap-4 justify-start'>
-            <h1 className='text-xl font-semibold'>Call Center</h1>{' '}
-            <p>+234 816 469 8085</p>
+            <h1 className='text-2xl font-semibold'>Call Center</h1>{' '}
+            <p className='font-medium text-xl'>+234 816 469 8085</p>
           </div>
         </div>
-        <div className='w-full lg:w-fit'>
+        <div className='w-full lg:w-fit lg:flex gap-5 items-center'>
           {' '}
+          <Image src={email} alt='' width={100} height={30} />
           <div className='flex flex-col gap-4'>
-            <h1 className='text-xl font-semibold'>Email Us</h1>{' '}
-            <p>onpointsynergyltd@gmail.com</p>
+            <h1 className='text-2xl font-semibold'>Email Us</h1>{' '}
+            <p className='font-medium text-xl'>onpointsynergyltd@gmail.com</p>
           </div>
         </div>
       </div>
