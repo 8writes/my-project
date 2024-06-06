eb18d980e753814b57e04d171e8620c98bba1458
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-06-06 13:32:44 +0100

    fix

--- src/views/terms-policy/terms-policy.jsx
+++ src/views/terms-policy/terms-policy.jsx
@@ -8,109 +8,100 @@ const TermsPolicy = () => {
     <div className='about bg-white pt-16 lg:pt-20'>
       <PagesHeader title='Terms & Conditions' />
       <div className='text-slate-800 p-5 pb-40 md:p-24'>
-        <p className='py-10'>Last updated: June 06, 2024.</p>
-        <p>
+        <p className='pb-10'>Last updated: January 06, 2019.</p>
+        <p className='pb-10'>
           Please read these terms and conditions carefully before using Our
           Service.
         </p>
 
-        <h2>Interpretation and Definitions</h2>
+        <h2 className='pb-5'>Interpretation and Definitions</h2>
 
-        <h3>Interpretation</h3>
-        <p>
+        <h3 className='pb-4'>Interpretation</h3>
+        <p className='pb-4'>
           The words with initial letters capitalized have meanings defined under
           the following conditions. These definitions shall have the same
           meaning regardless of whether they appear in singular or plural form.
         </p>
 
-        <h3>Definitions</h3>
-        <p>For the purposes of these Terms and Conditions:</p>
+        <h3 className='py-4'>Definitions</h3>
+        <p className='pb-4'>For the purposes of these Terms and Conditions:</p>
         <ul>
-          <li>
+          <li className='py-4'>
             <strong>Affiliate</strong> means an entity that controls, is
             controlled by, or is under common control with a party, where
             "control" means ownership of 50% or more of the shares, equity
             interest, or other securities entitled to vote for the election of
             directors or other managing authority.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Country</strong> refers to: Nigeria.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Company</strong> (referred to as either "the Company," "We,"
             "Us," or "Our" in this Agreement) refers to On-point Dynamics
             Synergy, 132 Woji Road GRA Phase II, Port Harcourt, Rivers State.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Device</strong> means any device that can access the Service
             such as a computer, a cellphone, or a digital tablet.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Service</strong> refers to the Website.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Terms and Conditions</strong> (also referred to as "Terms")
             mean these Terms and Conditions that form the entire agreement
             between You and the Company regarding the use of the Service.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Third-party Social Media Service</strong> means any services
             or content (including data, information, products, or services)
             provided by a third party that may be displayed, included, or made
             available by the Service.
           </li>
-          <li>
+          <li className='py-4'>
             <strong>Website</strong> refers to On-point Dynamics Synergy,
             accessible from{' '}
             <a href='https://www.onpointdynamicssynergy.com/'>
               https://www.onpointdynamicssynergy.com/
             </a>
           </li>
-          <li>
+          <li className='py-4'>
             <strong>You</strong> means the individual accessing or using the
             Service, or the company, or other legal entity on behalf of which
             such individual is accessing or using the Service, as applicable.
           </li>
         </ul>
 
-        <h2>Acknowledgment</h2>
-        <p>
+        <h2 className='py-4'>Acknowledgment</h2>
+        <p className='pb-4'>
           These are the Terms and Conditions governing the use of this Service
           and the agreement that operates between You and the Company. These
           Terms and Conditions set out the rights and obligations of all users
           regarding the use of the Service.
         </p>
-        <p>
+        <p className='py-4'>
           Your access to and use of the Service is conditioned on Your
           acceptance of and compliance with these Terms and Conditions. These
           Terms and Conditions apply to all visitors, users, and others who
           access or use the Service.
         </p>
-        <p>
+        <p className='py-4'>
           By accessing or using the Service, You agree to be bound by these
           Terms and Conditions. If You disagree with any part of these Terms and
           Conditions, then You may not access the Service.
         </p>
-        <p>
+        <p className='py-4'>
           You represent that you are over the age of 18. The Company does not
           permit those under 18 to use the Service.
         </p>
-        <p>
-          Your access to and use of the Service is also conditioned on Your
-          acceptance of and compliance with the Privacy Policy of the Company.
-          Our Privacy Policy describes Our policies and procedures on the
-          collection, use, and disclosure of Your personal information when You
-          use the Application or the Website and informs You about Your privacy
-          rights and how the law protects You. Please read Our Privacy Policy
-          carefully before using Our Service.
-        </p>
 
-        <h2>Links to Other Websites</h2>
-        <p>
+        <h2 className='py-4'>Links to Other Websites</h2>
+        <p className='py-4'>
           Our Service may contain links to third-party websites or services that
           are not owned or controlled by the Company.
         </p>
-        <p>
+        <p className='py-4'>
           The Company has no control over, and assumes no responsibility for,
           the content, privacy policies, or practices of any third-party
           websites or services. You further acknowledge and agree that the
@@ -119,23 +110,23 @@ const TermsPolicy = () => {
           connection with the use of or reliance on any such content, goods, or
           services available on or through any such websites or services.
         </p>
-        <p>
+        <p className='py-4'>
           We strongly advise You to read the terms and conditions and privacy
           policies of any third-party websites or services that You visit.
         </p>
 
-        <h2>Termination</h2>
-        <p>
+        <h2 className='py-4'>Termination</h2>
+        <p className='py-4'>
           We may terminate or suspend Your access immediately, without prior
           notice or liability, for any reason whatsoever, including without
           limitation if You breach these Terms and Conditions.
         </p>
-        <p>
+        <p className='py-4'>
           Upon termination, Your right to use the Service will cease
           immediately.
         </p>
 
-        <p>
+        <p className='py-4'>
           To the maximum extent permitted by applicable law, in no event shall
           the Company or its suppliers be liable for any special, incidental,
           indirect, or consequential damages whatsoever (including, but not
@@ -148,7 +139,7 @@ const TermsPolicy = () => {
           been advised of the possibility of such damages and even if the remedy
           fails of its essential purpose.
         </p>
-        <p>
+        <p className='py-4'>
           Some states do not allow the exclusion of implied warranties or
           limitation of liability for incidental or consequential damages, which
           means that some of the above limitations may not apply. In these
@@ -156,8 +147,8 @@ const TermsPolicy = () => {
           permitted by law.
         </p>
 
-        <h2>"AS IS" and "AS AVAILABLE" Disclaimer</h2>
-        <p>
+        <h2 className='py-4'>"AS IS" and "AS AVAILABLE" Disclaimer</h2>
+        <p className='py-4'>
           The Service is provided to You "AS IS" and "AS AVAILABLE" and with all
           faults and defects without warranty of any kind. To the maximum extent
           permitted under applicable law, the Company, on its own behalf and on
@@ -175,7 +166,7 @@ const TermsPolicy = () => {
           any performance or reliability standards, or be error-free or that any
           errors or defects can or will be corrected.
         </p>
-        <p>
+        <p className='py-4'>
           Without limiting the foregoing, neither the Company nor any of the
           company's providers makes any representation or warranty of any kind,
           express or implied: (i) as to the operation or availability of the
@@ -187,7 +178,7 @@ const TermsPolicy = () => {
           of the Company are free of viruses, scripts, trojan horses, worms,
           malware, timebombs, or other harmful components.
         </p>
-        <p>
+        <p className='py-4'>
           Some jurisdictions do not allow the exclusion of certain types of
           warranties or limitations on applicable statutory rights of a
           consumer, so some or all of the above exclusions and limitations may
@@ -196,40 +187,24 @@ const TermsPolicy = () => {
           enforceable under applicable law.
         </p>
 
-        <h2>Governing Law</h2>
-        <p>
+        <h2 className='py-4'>Governing Law</h2>
+        <p className='py-4'>
           The laws of the Country, excluding its conflicts of law rules, shall
           govern these Terms and Your use of the Service. Your use of the
           Application may also be subject to other local, state, national, or
           international laws.
         </p>
 
-        <h2>Disputes Resolution</h2>
-        <p>
+        <h2 className='py-4'>Disputes Resolution</h2>
+        <p className='py-4'>
           If You have any concern or dispute about the Service, You agree to
           first try to resolve the dispute informally by contacting the Company.
         </p>
 
-        <h2>For European Union (EU) Users</h2>
-        <p>
-          If You are a European Union consumer, you will benefit from any
-          mandatory provisions of the law of the country in which You are
-          resident.
-        </p>
+        <h2 className='py-4'>Severability and Waiver</h2>
 
-        <h2>United States Legal Compliance</h2>
-        <p>
-          You represent and warrant that (i) You are not located in a country
-          that is subject to a United States government embargo, or that has
-          been designated by the United States government as a "terrorist
-          supporting" country, and (ii) You are not listed on any United States
-          government list of prohibited or restricted parties.
-        </p>
-
-        <h2>Severability and Waiver</h2>
-
-        <h3>Severability</h3>
-        <p>
+        <h3 className='py-4'>Severability</h3>
+        <p className='py-4'>
           If any provision of these Terms is held to be unenforceable or
           invalid, such provision will be changed and interpreted to accomplish
           the objectives of such provision to the greatest extent possible under
@@ -237,8 +212,8 @@ const TermsPolicy = () => {
           force and effect.
         </p>
 
-        <h3>Waiver</h3>
-        <p>
+        <h3 className='py-4'>Waiver</h3>
+        <p className='py-4'>
           Except as provided herein, the failure to exercise a right or to
           require performance of an obligation under these Terms shall not
           affect a party's ability to exercise such right or require such
@@ -246,22 +221,22 @@ const TermsPolicy = () => {
           constitute a waiver of any subsequent breach.
         </p>
 
-        <h2>Translation Interpretation</h2>
-        <p>
+        <h2 className='py-4'>Translation Interpretation</h2>
+        <p className='py-4'>
           These Terms and Conditions may have been translated if We have made
           them available to You on our Service. You agree that the original
           English text shall prevail in the case of a dispute.
         </p>
 
-        <h2>Changes to These Terms and Conditions</h2>
-        <p>
+        <h2 className='py-4'>Changes to These Terms and Conditions</h2>
+        <p className='py-4'>
           We reserve the right, at Our sole discretion, to modify or replace
           these Terms at any time. If a revision is material, We will make
           reasonable efforts to provide at least 30 days' notice prior to any
           new terms taking effect. What constitutes a material change will be
           determined at Our sole discretion.
         </p>
-        <p>
+        <p className='py-4'>
           By continuing to access or use Our Service after those revisions
           become effective, You agree to be bound by the revised terms. If You
           do not agree to the new terms, in whole or in part, please stop using
