71bdce3e2e2e2ca886378fcb20970ec23e152eb3
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-06-19 15:49:32 +0100

    fix

--- src/views/projects/projects.jsx
+++ src/views/projects/projects.jsx
@@ -10,14 +10,58 @@ import image2 from '../../../public/images/reconstroction.jpg'
 import image3 from '../../../public/images/renovation.jpg'
 import PagesHeader from '@/components/pages-header'
 
-import images3_1 from '../../../public/images/renovation.jpg'
-import images3_2 from '../../../public/images/renovation.jpg'
-import images3_3 from '../../../public/images/renovation.jpg'
+import images3_1 from '../../../public/projects/IMG-20240418-WA0071.jpg'
+import images3_2 from '../../../public/projects/IMG-20240418-WA0072.jpg'
+import images3_3 from '../../../public/projects/IMG-20240418-WA0073.jpg'
+import images3_4 from '../../../public/projects/IMG-20240418-WA0074.jpg'
+import images3_5 from '../../../public/projects/IMG-20240418-WA0075.jpg'
+import images3_6 from '../../../public/projects/IMG-20240418-WA0076.jpg'
+import images3_7 from '../../../public/projects/IMG-20240418-WA0087.jpg'
+import images3_8 from '../../../public/projects/IMG-20240418-WA0077.jpg'
+
+import images2_1 from '../../../public/projects/IMG-20240418-WA0079.jpg'
+import images2_2 from '../../../public/projects/IMG-20240418-WA0080.jpg'
+import images2_3 from '../../../public/projects/IMG-20240418-WA0081.jpg'
+import images2_4 from '../../../public/projects/IMG-20240418-WA0082.jpg'
+import images2_5 from '../../../public/projects/IMG-20240418-WA0083.jpg'
+import images2_7 from '../../../public/projects/IMG-20240418-WA0085.jpg'
+
+import images1_1 from '../../../public/projects/IMG-20240418-WA0089.jpg'
+import images1_2 from '../../../public/projects/IMG-20240418-WA0090.jpg'
+import images1_3 from '../../../public/projects/IMG-20240418-WA0091.jpg'
+import images1_4 from '../../../public/projects/IMG-20240418-WA0092.jpg'
+import images1_5 from '../../../public/projects/IMG-20240418-WA0093.jpg'
+import images1_6 from '../../../public/projects/IMG-20240418-WA0094.jpg'
+import images1_7 from '../../../public/projects/IMG-20240418-WA0095.jpg'
 
 const Projects = () => {
-  const images1 = [images3_1, images3_2, images3_3]
-  const images2 = [images3_1, images3_2, images3_3]
-  const images3 = [images3_1, images3_2, images3_3]
+ const images1 = [
+   images1_1,
+   images1_2,
+   images1_3,
+   images1_4,
+   images1_5,
+   images1_6,
+   images1_7,
+ ]
+ const images2 = [
+   images2_1,
+   images2_2,
+   images2_3,
+   images2_4,
+   images2_5,
+   images2_7,
+ ]
+ const images3 = [
+   images3_1,
+   images3_2,
+   images3_3,
+   images3_4,
+   images3_5,
+   images3_6,
+   images3_7,
+   images3_8,
+ ]
   return (
     <div className='min-h-screen projects-page bg-white pt-16 lg:pt-20'>
       <PagesHeader title='Projects' />
